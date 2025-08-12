cef28d770ee6a13fb3dd52d16edf6c95c3a2d8a2
Kiran Raj R <kiranrajvjd@gmail.com>
2025-08-12 21:11:57 +0530

    Concatenation of Array v1
    Date: 12/08/2025
    Question: 1
    Level: Easy
    Code: Python
    Question: Concatenation of Array

---
+++ Neetcode/1_array_concatenation.py
@@ -0,0 +1,14 @@
+# You are given an integer array nums of length n. 
+# Create an array ans of length 2n where ans[i] == nums[i] 
+# and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+# Specifically, ans is the concatenation of two nums arrays.
+# Return the array ans.
+
+from typing import List
+def getConcatenation(nums: List[int]) -> List[int]:
+    nums.extend(nums)
+    return nums
+
+numbers = [1,2,3,4,5]
+print(getConcatenation(numbers))    
+# Output: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
