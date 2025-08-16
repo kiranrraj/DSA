d67e4074a2d25349db44c0ce3d6a3dd3b36da3e2
Kiran Raj R <kiranrajvjd@gmail.com>
2025-08-16 14:01:15 +0530

    Get majority element (Randomization Version 2)
    # Date: 16/08/2025
    # Question: 6
    # Level: Easy
    # Code: Python
    # Question: Get majority element (Randomization Version 2)

---
+++ Neetcode/6_majority_element_v2.py
@@ -0,0 +1,26 @@
+# Date: 16/08/2025
+# Question: 6
+# Level: Easy
+# Code: Python
+# Question: Given an array nums of size n, return the majority element. The majority element is the element 
+# that appears more than ⌊n / 2⌋ times in the array. You may assume that the majority element always exists in the array.
+
+# The Boyer-Moore Voting Algorithm finds the majority element by leveraging the fact that the majority element's 
+# frequency is greater than the combined frequency of all other elements. The algorithm works by pairing each 
+# occurrence of a non-majority element with a single occurrence of the majority element, effectively cancelling 
+# them out. Since the majority element appears more than half the time, it will be the last one standing with a positive count.
+
+# Randomization
+import random
+def majorityElement(nums):
+    n = len(nums)
+    while True:
+        candidate = random.choice(nums)
+        if nums.count(candidate) > n // 2:
+            return candidate
+
+nums1 = [4,4,4,2,3,3,3,4,4,4,4,4,4,3,3,3]
+print(majorityElement(nums1))
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
