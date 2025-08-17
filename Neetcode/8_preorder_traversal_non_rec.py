b8aea02a03d10c1a01b1a62e51aa79c747f0128a
Kiran Raj R <kiranrajvjd@gmail.com>
2025-08-17 16:55:03 +0530

    Preorder Traversal [iterative]
    # Date: 17/08/2025
    # Question: 8
    # Level: Easy
    # Code: Python
    # Question: Preorder Traversal [iterative]

---
+++ Neetcode/8_preorder_traversal_non_rec.py
@@ -0,0 +1,42 @@
+# Date: 17/08/2025
+# Question: 8
+# Level: Easy
+# Code: Python
+# Question: Preorder Traversal [iterative]
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def preorder_iterative(root):
+    if not root:
+        return []
+
+    res = []
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+        res.append(node.val)
+        print(res)
+        
+        # push right first so left is processed next (LIFO)
+        if node.right:
+            stack.append(node.right) 
+        if node.left:
+            stack.append(node.left) 
+    return res
+
+if __name__ == "__main__":
+    root = TreeNode(
+        1,
+        TreeNode(2, TreeNode(4), TreeNode(5)),
+        TreeNode(3)
+    )
+
+    print("Steps:")
+    order = preorder_iterative(root)
+    print("Final:")
+    print(order)
